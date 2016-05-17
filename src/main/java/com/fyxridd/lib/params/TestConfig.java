a7fe800a5b0f7ce8299670ecceb40634e8180ac2
yuexiangfeng <fyxridd@163.com>
2016-05-17 14:19:29 +0800

    更新

--- src/main/java/com/fyxridd/lib/params/TestConfig.java
+++ src/main/java/com/fyxridd/lib/params/TestConfig.java
@@ -2,7 +2,6 @@ package com.fyxridd.lib.params;
 
 import com.fyxridd.lib.config.api.basic.Path;
 import com.fyxridd.lib.config.api.convert.ConfigConvert;
-import com.fyxridd.lib.params.api.Format;
 import com.fyxridd.lib.params.api.FormatConverter;
 import com.fyxridd.lib.params.api.ValueGetter;
 
